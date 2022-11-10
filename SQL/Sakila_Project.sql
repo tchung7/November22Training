299831be332e3593395ac3f0c71c1b842b5a0fdb
tristanchung <tristan.chung@docme.health>
2022-11-09 16:36:03 -0800

    more edits to sql file

--- SQL/Sakila_Project.sql
+++ SQL/Sakila_Project.sql
@@ -12,18 +12,13 @@ select * from payment as p join customer as c on p.customer_id = c.customer_id w
 
 create view bob_fawcett_films as select f.* from film as f join film_actor as fa on fa.film_id = f.film_id join actor as a on a.actor_id = fa.actor_id where a.first_name = 'Bob' and a.last_name = 'Fawcett';
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `GetAlienCenterInventory`()
-begin
-	select inventory_id from inventory as i join film as f on i.film_id = f.film_id where f.title = 'Alien Center' and i.store_id = 2;
-end
-
-call GetAlienCenterInventory;
-
 start transaction;
 
-insert into staff values(3,'Tristan','Chung',5, null, 'tristanchung97@gmail.com', 1,1,'Tristan', null, now());
+insert into staff values(3,'Tristan','Chung',5, null, 'tristanchung97@gmail.com', 0,1,'Tristan', null, now());
 insert into store values(3,3,3, now());
 commit;
+
+delete from store where store_id = 3;
 
 select count(*) as '# of movies', rating from film group by rating order by 1 DESC;
 
